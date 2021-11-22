4cca7ae35a5e8dc8b7897155615f5c46e8831c3c
Robertto Flores <roberttflores042@gmail.com>
2021-11-21 17:09:31 -0800

    Initial Commit

---
+++ StoreCh/StoreChApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreChApp.swift
+//  StoreCh
+//
+//  Created by Robertto Flores on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct StoreChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
